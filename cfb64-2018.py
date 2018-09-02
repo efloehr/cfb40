ee27dcd4ffc492dae6b5bfafedfd1f3c5d14bcd1
Eric Floehr <eric@intellovations.com>
2018-09-02 12:59:56 -0400

    Remove ix deprecation warning

--- cfb64-2018.py
+++ cfb64-2018.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import itertools
 import sys
 
-CFB40_MAX = OrderedDict([
+CFB64_MAX = OrderedDict([
 
     ('Grimm', ['Alabama','Clemson','FSU','Georgia','Kansas','OSU','Oregon St','Pitt']),
     ('Eric', ['Auburn','Baylor','Clemson','Florida','Mich. St.','Notre Dame','Texas','Washington']),
@@ -13,7 +13,7 @@ CFB40_MAX = OrderedDict([
     ('CompuNick', ['Alabama','Baylor','Cal','Clemson','Florida','OSU','Texas','Washington']),
 ])
 
-CFB40_MIN = OrderedDict([
+CFB64_MIN = OrderedDict([
     ('Grimm', ['Illinois','Indiana','Louisville','Maryland','Northwestern','Notre Dame','Oklahoma','Wisconsin']),
     ('Eric', ['Arizona State','Colorado','Kentucky','LSU','Northwestern','TCU','UVA','Washington St']),
     ('Nick', ['Kansas','LSU','Northwestern','Oregon St','S Carolina','TCU','USC','Wisconsin']),
@@ -35,12 +35,12 @@ cfb_data = make_cfb_data(data)
 def print_win_diff(team_diffs, current_data, previous_data):
     for team, diff in team_diffs:
         print("{0:<16} [{1:>2d}-{2:>2d}] {3:>4.1f} -> {4:>4.1f} (net {5:+4.1f})".format(
-            team, current_data.ix[team]['wins'], current_data.ix[team]['losses'],
-            previous_data.ix[team]['projected_wins'], current_data.ix[team]['projected_wins'], diff))
+            team, current_data.loc[team]['wins'], current_data.loc[team]['losses'],
+            previous_data.loc[team]['projected_wins'], current_data.loc[team]['projected_wins'], diff))
 
 
 if week > 0:
-    proj_diff = {team: cfb_data.ix[team]['projected_wins'] - last_week_data.ix[team]['projected_wins'] for team in cfb_data.index}
+    proj_diff = {team: cfb_data.loc[team]['projected_wins'] - last_week_data.loc[team]['projected_wins'] for team in cfb_data.index}
     sorted_diff = [(team, proj_diff[team]) for team in sorted(proj_diff, key=proj_diff.get)]
     print("Top 10 decreases in win projections (last week):")
     print("-------------------------------------------------")
@@ -51,7 +51,7 @@ if week > 0:
     print_win_diff(reversed(sorted_diff[-10:]), cfb_data, last_week_data)
     print("")
 
-    proj_diff = {team: cfb_data.ix[team]['projected_wins'] - preseason_data.ix[team]['projected_wins'] for team in cfb_data.index}
+    proj_diff = {team: cfb_data.loc[team]['projected_wins'] - preseason_data.loc[team]['projected_wins'] for team in cfb_data.index}
     sorted_diff = [(team, proj_diff[team]) for team in sorted(proj_diff, key=proj_diff.get)]
     print("Top 10 decreases in win projections (this season):")
     print("--------------------------------------------------")
@@ -62,16 +62,16 @@ if week > 0:
     print_win_diff(reversed(sorted_diff[-10:]), cfb_data, preseason_data)
     print("")
 
-#best_cfb_max, best_cfb_min = calculate_best_cfbNmax_and_min_teams(cfb_data, 8)
-
 print("Max Standings:")
 print("--------------")
 print("")
-for name, teams in CFB40_MAX.items():
+for name, teams in CFB64_MAX.items():
     print("{0}'s Entries:".format(name))
     print("----------------------------------------------------------------------------")
     print_selection(cfb_data, last_week_data, preseason_data, teams)
     print("")
+
+best_cfb_max, best_cfb_min = calculate_best_cfbNmax_and_min_teams(cfb_data, 8)
 
 print("Best Projected Entry:")
 print("----------------------------------------------------------------------------")
@@ -82,7 +82,7 @@ print("")
 print("Min Standings:")
 print("--------------")
 print("")
-for name, teams in CFB40_MIN.items():
+for name, teams in CFB64_MIN.items():
     print("{0}'s Entries:".format(name))
     print("----------------------------------------------------------------------------")
     print_selection(cfb_data, last_week_data, preseason_data, teams)
