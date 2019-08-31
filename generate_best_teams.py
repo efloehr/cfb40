f0dea9e08d71a22f13bfc45dbd73dd04454b29a0
Eric Floehr <eric@intellovations.com>
2019-08-31 08:43:16 -0400

    Improve output

--- generate_best_teams.py
+++ generate_best_teams.py
@@ -1,9 +1,11 @@
 from data import get_cfb_data
 from cfbN import calculate_best_cfbNmax_and_min_teams, print_selection, make_cfb_data
 import itertools
+import collections
 
 data = get_cfb_data(2019, 0)
 num_teams = 8
+num_to_keep = 20
 
 cfbNframe = make_cfb_data(data)
 cfbNdict = cfbNframe.to_dict(orient='index')
@@ -14,10 +16,12 @@ cfbN_num = 0
 
 # Get all combinations of n teams with 8n wins or less
 cfbNmax_selection = None
+cfbNmax_best = collections.deque(maxlen=num_to_keep)
 cfbNmax_predicted_wins = -1
 cfbNmax_num = 0
 
 cfbNmin_selection = None
+cfbNmin_best = collections.deque(maxlen=num_to_keep)
 cfbNmin_predicted_wins = 999999
 cfbNmin_num = 0
 
@@ -26,18 +30,20 @@ n8wins = num_teams * 8
 for n_team in all_n_teams:
     cfbN_num += 1
     wins = sum([cfbNdict[team]['ovr_win'] for team in n_team])
-    proj_wins = sum([round(cfbNdict[team]['projected_wins']) for team in n_team])
+    proj_wins = sum([cfbNdict[team]['projected_wins'] for team in n_team])
     if wins <= n8wins:
         cfbNmax_num += 1
         if proj_wins > cfbNmax_predicted_wins:
             cfbNmax_predicted_wins = proj_wins
             cfbNmax_selection = n_team
+            cfbNmax_best.append(n_team)
             print("Current MAX wins: {}".format(proj_wins))
     if wins >= n8wins:
         cfbNmin_num += 1
         if proj_wins < cfbNmin_predicted_wins:
             cfbNmin_predicted_wins = proj_wins
             cfbNmin_selection = n_team
+            cfbNmin_best.append(n_team)
             print("Current min wins: {}".format(proj_wins))
 
 print("Number of total team combinations: {}".format(cfbN_num))
@@ -53,3 +59,13 @@ print("----- min {} -----".format(cfbNmin_predicted_wins))
 print_selection(data, data, data, cfbNmin_selection)
 print("------------------")
 print("")
+
+print("########### TOP MAX ############")
+for team in cfbNmax_best:
+    print_selection(data, data, data, team)
+    print("")
+
+print("########### top min ############")
+for team in cfbNmin_best:
+    print_selection(data, data, data, team)
+    print("")
